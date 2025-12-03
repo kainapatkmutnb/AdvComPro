978a2ea006c69544b73b04991b5acde4223a5489
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-12-03 10:20:46 +0700

    เพิ่มฟังก์ชันสร้าง log entry พร้อม timestamp เวลาไทย และ event ID สุ่ม

--- Lecture-02 DataTypes/create-log.js
+++ Lecture-02 DataTypes/create-log.js
@@ -1,7 +1,7 @@
 function createLog(message) {
   // 1. สร้าง timestamp
   const now = new Date();
-  const timestamp = now.toISOString('th-TH'); // เช่น 2023-09-17T10:00:00
+  const timestamp = now.toLocaleTimeString('th-TH'); // เช่น 10:00:00
 
   // 2. สร้าง Event ID (Math + String)
   const eventId = Math.random().toString(16).substring(2, 10).toUpperCase();
@@ -9,9 +9,9 @@ function createLog(message) {
   // 3. จัดรูปแบบข้อความ (String)
   const logMessage = message.toUpperCase();
 
-  return `[${timestamp} [${eventId}] [${logMessage}]`;
+  return `[${timestamp} [${eventId}] - ${logMessage}`;
 }
 
 const log = createLog("User Login Successful");
 console.log(log);
-// Output: (ตัวอย่าง) [2023-09-17T10:00:00 [12345678] [USER LOGIN SUCCESSFUL]]
+// Output: (ตัวอย่าง) [10:00:00 [12345678] - USER LOGIN SUCCESSFUL
