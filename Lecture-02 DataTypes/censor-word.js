b1b80192689bff1bfa0b25becd992016cf9c8dd8
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-12-03 09:40:46 +0700

    feat: add word censoring functionality

--- Lecture-02 DataTypes/censor-word.js
+++ Lecture-02 DataTypes/censor-word.js
@@ -2,12 +2,13 @@ function censorWord(sentence, wordToCensor) {
   // สร้าง RegExp object โดยมี 'gi' flag
   // 'g' = global (ทุกตำแหน่ง)
   // 'i' = case-insensitive (ไม่สนใจตัวพิมพ์เล็ก/ใหญ่)
-  const regex = new RegExp(wordToCensor, 'gi');
-  const censoredText = sentence.replace(regex, '***');
+  const regex = new RegExp(wordToCensor, "gi");
+  const censoredText = sentence.replace(regex, "***");
   return censoredText;
 }
 
-const originalPost = "JavaScript is fun, but sometimes javascript can be tricky.";
+const originalPost =
+  "JavaScript is fun, but sometimes javascript can be tricky.";
 const cleanPost = censorWord(originalPost, "javascript");
 
 console.log(cleanPost);
