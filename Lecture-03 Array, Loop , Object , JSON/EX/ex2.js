9489b4bbde9ed02f48a6b523a5e89dc1ccdc2985
kainapatkmutnb <s6506022620052@kmutnb.ac.th>
2025-12-24 09:57:16 +0700

    เพิ่มแบบฝึกหัดหาค่าซ้ำในหลาย Array (ex2.js)
    - สร้างไฟล์ EX/ex2.js
    - ใช้ for loop ตรวจสอบค่าที่อยู่ใน values1, values2, values3 พร้อมผลลัพธ์ใน result

---
+++ Lecture-03 Array, Loop , Object , JSON/EX/ex2.js
@@ -0,0 +1,13 @@
+let values1 = ["Apple", 1, false];
+let values2 = ["Fries", 2, true, "Apple"];
+let values3 = ["Mars", 9, "Apple"];
+
+// หาข้อมูลที่ซ้ำกันในทุก Array
+let result = [];
+for (let i = 0; i < values1.length; i++) {
+    // check ค่า ใน values1 มีอยู่ใน values2 และ values3 หรือไม่
+    if (values2.includes(values1[i]) && values3.includes(values1[i])) {
+        result.push(values1[i]);
+    }
+}
+console.log(result);
